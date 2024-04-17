ade3a4306a575f1c5a04b90b063c6d8dd5e660f7
Patrick Arminio <patrick.arminio@gmail.com>
2024-04-17 13:46:28 +0200

    Add reviewable interface

--- books/app.py
+++ books/app.py
@@ -1,40 +1,37 @@
 from typing import List
- 
+
 import strawberry
-from strawberry.federation.schema_directives import Requires, External
- 
- 
-# @strawberry.federation.type(keys=["id"])
-# @strawberry.federation.interface(keys=["id"], directives=[External()])
-@strawberry.federation.interface(keys=["id"],)
-class Book:
+
+
+@strawberry.federation.interface(keys=["id"])
+class Reviewable:
     id: strawberry.ID
+
+
+@strawberry.federation.type(
+    keys=["id"],
+)
+class Book(Reviewable):
     title: str
 
 
-    @classmethod
-    def resolve_reference(cls, id: strawberry.ID, title: str) -> 'Book':
-        # here we could fetch the book from the database
-        # or even from an API
-        return Book(id=id, title=title)
- 
- 
 def get_all_books() -> List[Book]:
     return Book(id=strawberry.ID("1"), title="The Dark Tower")
 
 
- 
 @strawberry.type
 class Query:
     all_books: List[Book] = strawberry.field(resolver=get_all_books)
 
     @strawberry.field
     def book(self) -> Book:
-        return Book(id=strawberry.ID("1"), title='A title')
- 
- 
-schema = strawberry.federation.Schema(
-    query=Query,
-    types=[Book], 
-    enable_federation_2=True)
+        return Book(id=strawberry.ID("1"), title="A title")
 
+    @strawberry.field
+    def reviewable(self) -> Reviewable:
+        return Book(id=strawberry.ID("1"), title="A title")
+
+
+schema = strawberry.federation.Schema(
+    query=Query, types=[Book], enable_federation_2=True
+)
